db700d7f71d265284cb4670aa0ad2efc68773cc8
Veli-Pekka Nurmi <nurmi.vp@gmail.com>
2022-04-18 11:26:38 +0300

    text etc updates

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -6,10 +6,11 @@ import _1 from "../assets/img/bg-lights.jpg";
 import _2 from "../assets/img/bg-room.jpeg";
 import _3 from "../assets/img/bg-sea.jpg";
 import _4 from "../assets/img/bg-wall.jpg";
+import _5 from "../assets/img/bg-leafs.jpeg";
 
 const Container = styled.div`
-  background-color: ${configData.THEME_COLORS.CYAN};
-  
+background-color: ${configData.THEME_COLORS.CYAN};
+
   h1 {
     /* color: #FFFFFF;
     font-family: 'Comforter Brush', cursive;
@@ -18,28 +19,27 @@ const Container = styled.div`
     transform: rotate(-10deg); */
   }
 
-  // background: linear-gradient(rgb(72,0,72,0.8), rgb(192,72,72,0.8)), url(${_4});
-  
   background-image: url(${_2});
   background-size: cover;
   background-repeat: no-repeat;
   // position: absolute; // if set, flows over the next content
   min-height: 100vh;
   min-width: 100vw;
-  background-position: center;
+  background-position: center; 
 `;
 
 // Technology, Marketing and Analytics
 
 export default function Main() {
   return (
-      <Container className="section main">
-        <h1>Veli-Pekka Nurmi</h1>
-        <p>
-          I'm a software developer with a performance marketing background. My
-          passion is to build and optimize web services that provide value to
-          users. I'm based in Helsinki, Finland.
-        </p>
-      </Container>
+    <Container className="section main">
+      <h1>Veli-Pekka Nurmi</h1>
+      <p>
+        I build and optimize web services where users love to return.
+      </p>
+    </Container>
   );
 }
+
+// I'm a software developer with a performance marketing background.
+// I'm based in Helsinki, Finland.
