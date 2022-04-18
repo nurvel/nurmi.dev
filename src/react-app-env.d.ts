d105ee14be0ea9264e7e09b74cb9adfb97966e8f
Veli-Pekka Nurmi <nurmi.vp@gmail.com>
2022-04-18 22:06:48 +0300

    new types to video files

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -4,3 +4,13 @@ declare module '*.mp4' {
     const src: string;
     export default src;
   }
+
+  declare module '*.ogm' {
+    const src: string;
+    export default src;
+  }
+
+  declare module '*.webm' {
+    const src: string;
+    export default src;
+  }
