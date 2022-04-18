b49ab317424447bea319529ed8b61346f577563b
Veli-Pekka Nurmi <nurmi.vp@gmail.com>
2022-04-18 20:52:51 +0300

    added header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  .header p {
+    color: #ffffff;
+    margin-left: 20px;
+    margin-top: 15px;
+    font-family: "Roboto Condensed", sans-serif;
+    font-size: 1rem;
+  }
+  .header {
+    position: fixed;
+    top: 0;
+    width: 100vw;
+    // background-color: #333;
+    z-index: 1;
+  }
+`;
+
+export default function Header() {
+  return (
+    <Container>
+      <div className="header">
+        <p>VELI-PEKKA NURMI</p>
+      </div>
+    </Container>
+  );
+}
