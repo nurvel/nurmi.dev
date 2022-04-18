96a09aed33b920cc984690636e8011036c498bc3
Veli-Pekka Nurmi <nurmi.vp@gmail.com>
2022-04-18 20:52:34 +0300

    added styles

--- src/common/globalStyles.tsx
+++ src/common/globalStyles.tsx
@@ -1,6 +1,11 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
+  html, body {
+    max-width: 100%;
+    overflow-x: hidden;
+  }
+
   body {
     margin: 0;
     padding: 0;
@@ -15,6 +20,57 @@ const GlobalStyle = createGlobalStyle`
   justify-content: center;
   align-items: center; */
   }
+
+  h1 {
+    color: #ffffff;
+    font-size: 5rem;
+    font-family: "Roboto Condensed", sans-serif;
+    font-weight: bold;
+    text-shadow: 0.1em 0.1em 0 hsl(9000 50% 20%);
+  }
+
+  h2 {
+    color: #ffffff;
+    font-size: 4rem;
+    font-family: "Roboto Condensed", sans-serif;
+    font-weight: bold;
+    text-shadow: 0.1em 0.1em 0 hsl(9000 50% 20%);
+  }
+
+  p {
+    color: #ffffff;
+    font-size: 1.5rem;
+    font-family: "Roboto Condensed", sans-serif;
+    font-weight:400;
+  }
+
+a:link,a:visited, a:active   {
+  color: #ffffff;
+  text-decoration: none;
+}
+
+a:hover {
+  color: #F92C85;
+  text-decoration: none;
+}
+
+@media only screen and (max-width: 700px) {
+  h1 {
+    font-size: 3rem;
+    text-shadow: 0.1em 0.1em 0 hsl(9000 50% 20%);
+  }
+
+  h2 {
+    font-size: 2rem;
+    text-shadow: 0.1em 0.1em 0 hsl(9000 50% 20%);
+  }
+  p {
+    font-size: 1rem;
+    font-weight:400;
+  }
+}
+
+
 
 `;
 
