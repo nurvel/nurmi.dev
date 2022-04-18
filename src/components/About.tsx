fcda535f5cb8277099b1d8e06ab1db785b9b27ac
Veli-Pekka Nurmi <nurmi.vp@gmail.com>
2022-04-18 20:51:07 +0300

    new about page

---
+++ src/components/About.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+import styled from "styled-components";
+import configData from "../common/config.json";
+
+const Container = styled.div`
+  background-color: ${configData.THEME_COLORS.DARK};
+
+  min-height: 100vh;
+  min-width: 100vw;
+
+  .introduction {
+    position: relative;
+    margin-top: 400px;
+    margin-left: 150px;
+    margin-right: 80px;
+    max-width: 900px;
+
+    // top: 0%;
+    left: 30%;
+    transform: translate(-50%, -50%);
+
+    @media only screen and (max-width: 700px) {
+      margin-top: 300px;
+      margin-left: 80px;
+    }
+  }
+
+  p {
+    line-height: 1.4;
+  }
+`;
+
+// Welcome back
+export default function About() {
+  return (
+    <Container className="about section">
+      <div className="introduction">
+        <h2>Nice to meet you.</h2>
+        <p>
+          My name is Veli-Pekka Nurmi. I'm software developer with a performance
+          marketing background. I use my skill set to create and optimize
+          engaging web services. During the day I work as the head of R&D in
+          SaaShop. On my free time I like to try out new technologies, hike with
+          my dog, do jogging and play console games. I live in Helsinki,
+          Finland.
+          <br />
+          <br />
+          <br />
+          <a
+            href="https://www.linkedin.com/in/veli-pekkanurmi"
+            target="_blank"
+            rel="noreferrer"
+          >
+            LinkedIn{" "}
+          </a>{" "}
+          |
+          <a href="https://github.com/nurvel" target="_blank" rel="noreferrer">
+            {" "}
+            GitHub{" "}
+          </a>{" "}
+          |
+          <a href="mailto:nurmi.vp@gmail.com" target="_blank" rel="noreferrer">
+            {" "}
+            Email{" "}
+          </a>
+        </p>
+      </div>
+    </Container>
+  );
+}
