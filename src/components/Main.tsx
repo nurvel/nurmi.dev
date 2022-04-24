946c0b91018dbf4bde8f7ed4d8837a14fcd5a9c1
Veli-Pekka Nurmi <nurmi.vp@gmail.com>
2022-04-24 18:39:54 +0300

    Button component extracted from Main view

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,15 +1,14 @@
 import React from "react";
 import configData from "../common/config.json";
 import styled from "styled-components";
-import AnchorLink from "react-anchor-link-smooth-scroll";
 
-// import _video from "../assets/img/bg-video.mp4";
-// import _video_mobile_mp4 from "../assets/img/bg-video-mobile.mp4";
-// import _video_mobile_ogm from "../assets/img/bg-video-mobile.ogm";
 import _video_mobile_webm from "../assets/img/bg-video-mobile.webm";
+import Button from "./Button";
 
 const Container = styled.div`
   background-color: ${configData.THEME_COLORS.DARK};
+  min-height: 100vh;
+  min-width: 100vw;
 
   .hero {
     text-align: center;
@@ -21,32 +20,11 @@ const Container = styled.div`
     min-width: 300px;
   }
 
-  .btn {
-    background-color: #ffffff;
-    padding: 20px 20px 20px 20px;
-    margin-top: 20px;
-    box-shadow: 8px 8px 0px 0px hsl(9000 50% 20%);
-    width: 150px;
-    position: absolute;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    font-weight: bold;
-
-    @media only screen and (max-width: 700px) {
-      padding: 10px 10px 10px 10px;
-      font-size: 0.8rem;
-      width: 100px;
-      box-shadow: 5px 5px 0px 0px hsl(9000 50% 20%);
-    }
+  .btnContainer {
+    display: flex;
+    align-items: center;
+    justify-content: center;
   }
-
-  // background-image: url();
-  /* background-size: cover;
-  background-repeat: no-repeat; */
-  // position: absolute; // if set, flows over the next content
-  min-height: 100vh;
-  min-width: 100vw;
-  // background-position: center;
 `;
 
 export default function Main() {
@@ -68,16 +46,14 @@ export default function Main() {
           zIndex: "0",
         }}
       >
-        {/* <source src={_video_mobile_mp4} type="video/mp4" /> */}
-        {/* <source src={_video_mobile_ogm} type="video/ogg" /> */}
         <source src={_video_mobile_webm} type="video/webm" />
         Your browser does not support the video tag.
       </video>
       <div className="hero">
         <h1>Let's develop web services users love returning to.</h1>
-        {/* <div className="btn"> */}
-          <AnchorLink className="btn" href="#about">READ MORE</AnchorLink>
-        {/* </div> */}
+        <div className="btnContainer">
+          <Button linkText={"READ MORE"} href={"#about"}></Button>
+        </div>
       </div>
     </Container>
   );
