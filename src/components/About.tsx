08b1b27eba91b84a1b39731e5426c265a1830aa3
Veli-Pekka Nurmi <33843131+nurvel@users.noreply.github.com>
2022-04-24 14:57:33 +0300

    Implement css grid to about -page (#10)
    * Added profile pic
    
    * added grid and profile pic
    
    * updated hero title

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,33 +1,50 @@
 import React from "react";
 import styled from "styled-components";
 import configData from "../common/config.json";
+import ProfilePic from "../assets/img/veli-pekka-nurmi-profile.png";
 
 const Container = styled.div`
   background-color: ${configData.THEME_COLORS.DARK};
-
   min-height: 100vh;
-  min-width: 100vw;
-
-  .introduction {
-    position: relative;
-    margin-top: 400px;
-    margin-left: 150px;
-    margin-right: 80px;
-    max-width: 900px;
-
-    // top: 0%;
-    left: 30%;
-    transform: translate(-50%, -50%);
-
-    @media only screen and (max-width: 700px) {
-      margin-top: 300px;
-      margin-left: 60px;
-      margin-right: 60px;
-    }
-  }
 
   p {
     line-height: 1.4;
+  }
+`;
+
+const GridContainer = styled.div`
+  position: relative;
+  margin-top: 100px;
+
+  // center content - to global level?
+  left: 50%;
+  transform: translate(-50%);
+
+  display: grid;
+  grid-template-columns: repeat(1, 2fr 1fr);
+  max-width: 1300px;
+  gap: 10px;
+  padding: 20px;
+
+  @media (max-width: 700px) {
+    margin-top: 50px;
+    grid-template-columns: minmax(300px, 1fr);
+    .gridItem:nth-of-type(2) {
+      order: -1;
+    }
+  }
+`;
+
+const ProfilePictureContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  @media (max-width: 700px) {
+    img {
+      width: auto;
+      height: 250px;
+    }
   }
 `;
 
@@ -35,37 +52,50 @@ const Container = styled.div`
 export default function About() {
   return (
     <Container className="about section" id="about">
-      <div className="introduction">
-        <h2>Nice to meet you.</h2>
-        <p>
-          My name is Veli-Pekka Nurmi. I'm a software developer with a
-          performance marketing background. I use my skill set to create and
-          optimize engaging web services.
-          <br />
-          <br />
-          During the day I work as the Head of R&D in SaaShop. In my free time,
-          I like to try out new technologies, hike with my spouse and dog, do
-          jogging and play console games. I live in Helsinki, Finland.
-          <br />
-          <br />
-          <br />
-          <a
-            href="https://www.linkedin.com/in/veli-pekkanurmi"
-            target="_blank"
-            rel="noreferrer"
-          >
-            LinkedIn
-          </a>
-          &nbsp; | &nbsp;
-          <a href="https://github.com/nurvel" target="_blank" rel="noreferrer">
-            GitHub
-          </a>
-          &nbsp; | &nbsp;
-          <a href="mailto:nurmi.vp@gmail.com" target="_blank" rel="noreferrer">
-            Email
-          </a>
-        </p>
-      </div>
+      <GridContainer>
+        <div className="gridItem">
+          <h2>Nice to meet you.</h2>
+          <p>
+            My name is Veli-Pekka Nurmi. I'm a software developer with a
+            performance marketing background. I use my skill set to create and
+            optimize engaging web services.
+            <br />
+            <br />
+            During the day I work as the Head of R&D in SaaShop. In my free
+            time, I like to try out new technologies, hike with my spouse and
+            dog, do jogging and play console games. I live in Helsinki, Finland.
+            <br />
+            <br />
+            <br />
+            <a
+              href="https://www.linkedin.com/in/veli-pekkanurmi"
+              target="_blank"
+              rel="noreferrer"
+            >
+              LinkedIn
+            </a>
+            &nbsp; | &nbsp;
+            <a
+              href="https://github.com/nurvel"
+              target="_blank"
+              rel="noreferrer"
+            >
+              GitHub
+            </a>
+            &nbsp; | &nbsp;
+            <a
+              href="mailto:nurmi.vp@gmail.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Email
+            </a>
+          </p>
+        </div>
+        <ProfilePictureContainer className="gridItem">
+          <img src={ProfilePic} />{" "}
+        </ProfilePictureContainer>
+      </GridContainer>
     </Container>
   );
 }
