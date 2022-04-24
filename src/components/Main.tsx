4751578224c8989498a7548a70aaa0ca51691b4c
Veli-Pekka Nurmi <nurmi.vp@gmail.com>
2022-04-24 14:52:28 +0300

    updated hero title

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -74,7 +74,7 @@ export default function Main() {
         Your browser does not support the video tag.
       </video>
       <div className="hero">
-        <h1>I develop web services users love returning to.</h1>
+        <h1>Let's develop web services users love returning to.</h1>
         {/* <div className="btn"> */}
           <AnchorLink className="btn" href="#about">READ MORE</AnchorLink>
         {/* </div> */}
