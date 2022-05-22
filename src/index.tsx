599585cf1d02c2779f09c1342fec3c64d665cb6c
Veli-Pekka Nurmi <33843131+nurvel@users.noreply.github.com>
2022-05-22 15:58:15 +0300

    Add gtm (#16)
    * install gtm
    
    * add GTM tag with environments

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import TagManager from "react-gtm-module";
+
+const envAuth =
+  process.env.NODE_ENV == "production"
+    ? "Uy1ZU2eirHiMidAl_yfBpQ"
+    : "CxNs4kxI_b1L4PUGBmhLXw";
+const envPreview = process.env.NODE_ENV == "production" ? "env-3" : "env-6";
+
+const tagManagerArgs = {
+  gtmId: "GTM-K4F7VVV",
+  auth: envAuth,
+  preview: envPreview,
+};
+TagManager.initialize(tagManagerArgs);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
