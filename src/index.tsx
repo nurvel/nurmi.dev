4fbbf171f130c134fbd5f745bdc9bee381181b69
Veli-Pekka Nurmi <nurmi.vp@gmail.com>
2024-08-09 08:35:46 +0300

    fixed linter warnings

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,10 @@ import App from "./App";
 import TagManager from "react-gtm-module";
 
 const envAuth =
-  process.env.NODE_ENV == "production"
+  process.env.NODE_ENV === "production"
     ? "Uy1ZU2eirHiMidAl_yfBpQ"
     : "CxNs4kxI_b1L4PUGBmhLXw";
-const envPreview = process.env.NODE_ENV == "production" ? "env-3" : "env-6";
+const envPreview = process.env.NODE_ENV === "production" ? "env-3" : "env-6";
 
 const tagManagerArgs = {
   gtmId: "GTM-K4F7VVV",
